879351c50776bc3dc56a1935863d7bbf60bed84f
wxlksy <31kiel12@gmail.com>
2025-01-25 16:06:05 +0300

    Update README.md

--- app/src/main/java/com/example/battletanks/drawers/GridDrawer.kt
+++ app/src/main/java/com/example/battletanks/drawers/GridDrawer.kt
@@ -30,7 +30,7 @@ class GridDrawer(private val container: FrameLayout)
         var leftMargin = 0
         while (leftMargin <= container!!.width)
         {
-            val verticalLine = View(container)
+            val verticalLine = View(container.context)
             val layoutParams = FrameLayout.LayoutParams (1, FrameLayout.LayoutParams.MATCH_PARENT)
             leftMargin += CELL_SIZE
             layoutParams.leftMargin = leftMargin
@@ -47,7 +47,7 @@ class GridDrawer(private val container: FrameLayout)
         var topMargin = 0
         while (topMargin <= container!!.height)
         {
-            val horizontalLine = View(container)
+            val horizontalLine = View(container.context)
             val layoutParams = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, 1)
             topMargin += CELL_SIZE
             layoutParams.topMargin = topMargin
